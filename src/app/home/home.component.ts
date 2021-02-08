00882989344a83e07eaca65e3d7bc6d525cdf2c8
Hector Romero <hectorromerodev@gmail.com>
2021-02-08 04:20:38 -0800

    UPD Home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,6 +10,18 @@ export class HomeComponent implements OnInit {
 
   constructor(private router: Router) { }
 
+  keep_learning = [
+    { thumbnail: '', avatar: '', course: 'Big Data', title: 'Curso de base de datos' },
+    { thumbnail: '', avatar: '', course: 'Como funciona react?', title: 'Curso de Front end con react.js' },
+    { thumbnail: '', avatar: '', course: 'Profesor: Emilio Consenza', title: 'Curso avanzado de UX Writing' }
+  ];
+
+  whats_new = [
+    { avatar: '', professor: 'Emiliano Consenza', title: 'Curso avanzado de UX Writing', badge: 'Nuevo' },
+    { avatar: '', professor: 'Samuel Burbano', title: 'Curso de Vue.js 3', badge: 'Nuevo' },
+    { avatar: '', professor: 'Emiliano Consenza', title: 'Curso de programacion programatica', badge: 'Nuevo' },
+  ];
+
   ngOnInit(): void { }
 
 }
